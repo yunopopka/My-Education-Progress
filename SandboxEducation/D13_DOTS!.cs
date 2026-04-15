da1d0550d905f70b0714c6fc40332a71a06bb138
plushie_yuno <2003.yaroslav777@gmail.com>
2026-04-15 13:08:08 +0300

    Create D13_DOTS!.cs

---
+++ SandboxEducation/D13_DOTS!.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Data/Drone")]
+
+public class DroneData : ScriptableObject
+{
+    public string droneName;
+    public float droneHp;
+    public Color droneCol;
+}
