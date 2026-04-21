6f1528bef87c26162f88bd65e6ea836dd9b47747
plushie_yuno <2003.yaroslav777@gmail.com>
2026-04-21 15:11:26 +0300

    Refactor comments and clean up code in D15_JSONSave.cs

--- SandboxEducation/D15_JSONSave.cs
+++ SandboxEducation/D15_JSONSave.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using System.IO; // Важно для работы с файлами!
+using System.IO; 
 
 public class SaveManager : MonoBehaviour
 {
@@ -7,37 +7,28 @@ public class SaveManager : MonoBehaviour
 
     private void Awake()
     {
-        // Формируем путь: "Папка_Игры/save.json"
         saveFilePath = Application.persistentDataPath + "/save.json"; 
     }
 
-    // --- СОХРАНЕНИЕ ---
     public void SaveGame(int waveToSave, float hpToSave)
     {
-        // 1. Упаковываем данные в контейнер
         SaveData data = new SaveData();
         data.currentWave = waveToSave;
         data.playerHealth = hpToSave;
 
-        // 2. Превращаем контейнер в строку JSON
         string json = JsonUtility.ToJson(data);
 
-        // 3. Записываем строку в файл на жесткий диск
         File.WriteAllText(saveFilePath, json);
         
         Debug.Log("Игра сохранена по пути: " + saveFilePath);
     }
 
-    // --- ЗАГРУЗКА ---
     public SaveData LoadGame()
     {
-        // Проверяем, существует ли вообще файл сохранения
         if (File.Exists(saveFilePath))
         {
-            // Читаем текст из файла
             string json = File.ReadAllText(saveFilePath);
             
-            // Расшифровываем JSON обратно в наш класс SaveData
             SaveData loadedData = JsonUtility.FromJson<SaveData>(json);
             return loadedData;
         }
